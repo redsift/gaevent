332eac6ee56101f4ea9cafc278be44291f4676f9
Christos Vontas <christos@redsift.io>
2017-02-08 17:13:58 +0000

    added revision flag

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var program = require('commander')
 
 
 program
-	.usage('<gaid> <category> <event>')
-	.option('-u --user', 'user name defaults to no-one')
-	.parse(process.argv);
+  .usage('<gaid> <category> <event>')
+  .option('-u, --user', 'user name defaults to no-one')
+  .option('-r, --revision <semver>', 'relevant software version for events')
+  .parse(process.argv);
 
 var visitor = ua( program.args[0], program.user, {strictCidFormat: false});
 
-visitor.event(program.args[1], program.args[2]).send()
+var version = program.revision ? ['version', program.revision].join(':') : '';
+
+visitor.event(program.args[1], program.args[2], version).send()
